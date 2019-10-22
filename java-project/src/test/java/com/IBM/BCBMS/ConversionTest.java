1d9b0a4d10b697dc02cdd380b707c701941ba8d7
root <root@ip-172-31-0-114.ap-south-1.compute.internal>
2019-10-22 12:50:45 +0000

    new java file added

---
+++ java-project/src/test/java/com/IBM/BCBMS/ConversionTest.java
@@ -0,0 +1,15 @@
+package com.IBM.BCBMS;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+public class ConversionTest{
+	
+	@Test
+	public void testTempConversion() throws Throwable{
+	   Conversion underTest = new Conversion();
+	   double temperature = 80.0d;
+	   String unit = "";
+	   double result = underTest.tempConversion(temperature, unit);
+           assertEquals(176.0d, result, 0.0);
+	}
+}
