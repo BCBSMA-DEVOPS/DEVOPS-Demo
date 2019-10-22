1dc1d6a9253559fa8ee93c05294533d70038cfba
Ambruth <ambruthkallubavi@in.ibm.com>
2019-10-22 13:14:50 +0530

    Update App.java

--- java-project2/src/main/java/com/IBM/BCBMS/App.java
+++ java-project2/src/main/java/com/IBM/BCBMS/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!_testing" );
+        System.out.println( "Hello World!_testing_Verify" );
     }
 }
