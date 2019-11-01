a743d2efde7aef5e7d06621b2f4d8f35600de92f
Ambruth Kallubavi <ambruthkallubavi@in.ibm.com>
2019-11-01 18:04:09 +0530

    updates to jmx file

---
+++ mavenwebproject/src/test/java/mavenwebproject/SimpleServletTest.java
@@ -0,0 +1,34 @@
+package mavenwebproject;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SimpleServletTest {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void beforeTest() {
+		System.setProperty("webdriver.gecko.driver", "C:\\IBM\\BCBSMA\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	    driver = new FirefoxDriver();  
+	}		
+	
+		
+	@Test
+	public void setUp() {
+			//Navigate to desired web page
+		
+		driver.get("http://localhost:8081/mavenwebproject/hello");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Hello World! Maven Web Project Example."));
+	}
+	
+	@After
+	public void afterTest() {
+		driver.quit();			
+	}
+}
